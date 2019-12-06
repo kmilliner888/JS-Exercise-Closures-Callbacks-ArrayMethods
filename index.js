517f00b101d0a838e4e5dd3b69b32be68b2f6c0d
Kaylen Milliner <kaylen.milliner@gmail.com>
2019-12-06 00:03:20 -0500

    Passed tests on first 3 challenges

--- index.js
+++ index.js
@@ -48,8 +48,8 @@ function processFirstItem(stringList, callback) {
  * [2] Invoking `processLength` passing `[]` and `(num) => "There are " + num`,
  * should return "There are 0".
 */
-function processLength(/* CODE HERE */) {
-  /* CODE HERE */
+function processLength(list, callback) {
+  return callback(list.length);
 }
 
 /**
@@ -66,8 +66,8 @@ function processLength(/* CODE HERE */) {
  * Invoking `processLastItem` passing `['foo', 'bar']` and `(str) => str + str`,
  * should return 'barbar'.
 */
-function processLastItem(/* CODE HERE */) {
-  /* CODE HERE */
+function processLastItem(stringList, callback) {
+  return callback(stringList[stringList.length-1]);
 }
 
 /**
@@ -87,9 +87,18 @@ function processLastItem(/* CODE HERE */) {
  * [2] Invoking `processSum` passing `[]` and `(num) => num + 1000`,
  * should return 1000.
 */
-function processSum(/* CODE HERE */) {
-  /* CODE HERE */
+function processSum(numberList, callback) {
+  var sum = numberList.reduce(function(sum, currentValue) {
+    return sum + currentValue;
+  }, 0);
+  callback(sum);
+};
+
+function outputSum(sum) {
+  console.log("The sum is" + sum);
 }
+
+console.log(processSum(numberList, outputSum));
 
 /**
  * ### Challenge `processProduct`
